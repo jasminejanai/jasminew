4213872685a8ae4498107e3b82492b27f0e2acce
QuanLe <quan.le1926@hotmail.com>
2014-11-13 11:15:12 +0900

    First commit.

---
+++ src/com/leapmotion/utilities/Common.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.leapmotion.utilities;
+
+/**
+ * @author Le Hong Quan
+ * @author Johan Gusten
+ * @version 1.0
+ *
+ */
+public class Common {
+
+}
---
+++ src/com/leapmotion/utilities/Constants.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.leapmotion.utilities;
+
+/**
+ * @author Le Hong Quan
+ * @author Johan Gusten
+ * @version 1.0
+ *
+ */
+public class Constants {
+
+    // Base
+    public static final int PERMIT_DEGREES = 90;
+    public static final String LEAP_INIT = "Initialized";
+    public static final String LEAP_CONNECTED = "Connected";
+    public static final String LEAP_DISCONNECTED = "Disconnected";
+    public static final String LEAP_EXIT = "Exited";
+
+    // Leap states
+    public static final String LEAP_INIT_STATE = "1 - Initial state";
+    public static final String LEAP_INTERMEDIATE_STATE = "2 - Intermediate state";
+    public static final String LEAP_FINAL_STATE = "3 - Final state";
+    public static final String LEAP_UNKNOWN_STATE = "Unknown";
+
+}
